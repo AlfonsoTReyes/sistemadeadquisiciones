13b1c8e26648e10947dd4da12a3ba5952a68ae59
evelyn <mhevelyn21@gmail.com>
2025-03-24 13:28:44 -0600

    ajustes sobre el proyecto en cuestión de la sub barra de navegación o el icono

--- sistemadeadquisiciones/src/app/pie.tsx
+++ sistemadeadquisiciones/src/app/pie.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-//import Link from 'next/link';
-//import { useState } from 'react';
-//import logo from '../public/images/logo_white.png';
+import Link from 'next/link';
+import { useState } from 'react';
+import logo from '../public/images/logo_white.png';
 
 
 export default function Pie() {
@@ -10,8 +10,7 @@ export default function Pie() {
   return (
     <nav className="bg-custom-color text-white w-full p-10">
       <div className="flex justify-center items-center">
-      {/*<img src={logo.src}  alt="Logo" style={{ height: '120px', width: 'auto' }}/>*/}
-      <label htmlFor="">LOGO</label>
+      <img src={logo.src}  alt="Logo" style={{ height: '120px', width: 'auto' }}/>
         <p className="text-center"> © 2024-2027 Municipio de San Juan del Río. </p>
         <br />
         <p className='text-center'>Todos los derechos reservados</p>
